bc8cbdede0654029194045c8ff5e83c279f02c68
karimzade_k <karimzade_k@STHQ012D-01>
2026-02-22 14:35:56 +0400

    SwagggerGen-e authorize elave edildi

--- Presentation/EShop.WebAPI/Program.cs
+++ Presentation/EShop.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using EShop.Application;
 using EShop.Persistence;
 using EShop.Infrastructure;
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,35 @@ builder.Services.AddControllers()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT tokeni daxil edin. M?s?l?n: Bearer 12345abcdef"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 
 builder.Services.AddApplicationRegister();
